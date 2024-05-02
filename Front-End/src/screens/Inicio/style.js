30128abbb155f8e617cdf82f1ee080c4da9180e9
Geovana Moreira <132011025+GeoGustin@users.noreply.github.com>
2024-05-02 15:36:50 -0300

    Update

---
+++ Front-End/src/screens/Inicio/style.js
@@ -0,0 +1,81 @@
+import { StyleSheet } from 'react-native'
+import { theme } from '../../theme'
+
+// criando  a estilização do componente de acordo com as informações passadas por parâmetro.
+//  TELA HOME
+export const stylesHomeScreen = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: theme.colors.ginger_cream,
+        padding: theme.spacing.lg, // espaçamento entre os componentes internos
+
+        //alignItems: 'center', 
+        justifyContent: 'space-evenly', // alinhamento dos itens
+    },
+
+})
+export const stylesWelcomeText = StyleSheet.create({
+    text: {
+        fontFamily: theme.font_family.regular,
+        fontSize: theme.font_size.xl,
+        color: theme.colors.chinese_black,
+        marginBottom: theme.spacing.lg,
+        textAlign: 'center',
+    },
+})
+export const stylesMainView = StyleSheet.create({
+    container: {
+        backgroundColor: theme.colors.peach_nougat,
+        padding: theme.spacing.lg,
+        borderRadius: theme.borderRadius.radius,
+    },
+    text: {
+        fontFamily: theme.font_family.regular,
+        fontSize: theme.font_size.xl,
+        color: theme.colors.white,
+        marginBottom: theme.spacing.xs,
+        textAlign: 'center',
+    },
+})
+
+export const stylesPressableStart = StyleSheet.create({
+    container: {
+        backgroundColor: theme.colors.marquis_orange,
+        paddingHorizontal: theme.spacing.lg,
+        paddingVertical: theme.spacing.sm,
+        borderRadius: theme.borderRadius.radius,
+    },
+    text: {
+        fontFamily: theme.font_family.regular,
+        fontSize: theme.font_size.xl,
+        color: theme.colors.white,
+        textAlign: 'center',
+    } 
+})
+
+// Botão Lixeira Delete
+// export const stylesButtonDelete = StyleSheet.create({
+//     button: {
+//         //borderRadius: theme.borderRadius.button,
+//     },
+//     image:{
+//     //resizeMode: 'contain',
+//         height:25, 
+//         width:25,
+//     },
+
+//     // ios
+//     shadowProp: {
+//         shadowColor: '#171717',
+//         shadowOffset: {width: -2, height: 4},
+//         shadowOpacity: 0.2,
+//         shadowRadius: 3,
+//     },    
+// })
+// //    |
+// // Botões
+// export const stylesButtonImage = StyleSheet.create({
+//     // padding:{
+//     // paddingLeft: theme.spacing.sm,
+//     // },   
+// })
---
+++ Front-End/src/theme/index.js
@@ -0,0 +1,32 @@
+export const theme = {
+    colors: {
+        wine_red: '#6A041D',
+        marquis_orange: '#D27238',
+        brown_yellow: '#DD9367',
+        peach_nougat: '#E7B495',
+        ginger_cream: '#F0E2D9',
+        white: '#FFFFFF',
+        chinese_black: '#141301',
+    },
+    font_family: {
+        regular: 'Karla_400Regular'
+    },
+    font_size: {
+        xl: 28,
+        lg: 20,
+        md: 16,
+        sm: 14,
+        xs: 12,
+        xxs: 10,
+    },
+    spacing: {
+        xl: 40,
+        lg: 30,
+        md: 20,
+        sm: 10,
+        xs: 5,
+    },
+    borderRadius: {
+        radius: 15,
+    }
+}

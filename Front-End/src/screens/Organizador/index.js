7681d06c259246adec2fa99d7dfa9c6c901f49b6
GeoGustin <geogustinad@gmail.com>
2024-05-09 09:50:11 -0300

    Feat: add icon

--- Front-End/src/screens/Organizador/index.js
+++ Front-End/src/screens/Organizador/index.js
@@ -19,6 +19,9 @@ import {
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 // inserção de área segura do dispositivo para Android e iOS.
 
+// Icon
+import { ChevronRight } from 'lucide-react-native';
+
 import * as S from './style.js'
 import GuestComponent from '../../components/GuestComponent/index.js';
 import OrganizerComponents from '../../components/OrganizerComponents/index.js';
@@ -51,44 +54,46 @@ const OrganizerScreen = ({navigation}) => {
     return (
     
         <SafeAreaProvider style={S.stylesOrganizerScreen.container}>
-        <ScrollView>
+          <ScrollView>
 
-          <View>
-            <Text style={S.stylesMainTitle.text}>Faça seu Cálculo</Text>
-          </View>
+            <View>
+              <Text style={S.stylesMainTitle.text}>Faça seu Cálculo</Text>
+            </View>
 
-          
-          {/* Organizador */}
-          <View style={S.stylesField.container}>
-            <Text style={S.stylesField.title}>{'Organizador(a):'}</Text>
-            <OrganizerComponents width={'51%'} text={'Quem irá organizar?'} value={organizador} maxLen={20}/>
-          </View>
+            
+            {/* Organizador */}
+            <View style={S.stylesField.container}>
+              <Text style={S.stylesField.title}>{'Organizador(a):'}</Text>
+              <OrganizerComponents width={'51%'} text={'Quem irá organizar?'} value={organizador} maxLen={20}/>
+            </View>
 
-          {/* Convidados */}
-          <View style={S.stylesField.container}>
-            <Text style={S.stylesField.title}>Convidados:</Text>
-            <GuestComponent src={source.homem} text={'Qtd. de homens'} value={adultos.homens} maxLen={4} inputMode={'numeric'}/>
-            <GuestComponent src={source.mulher} text={'Qtd. de mulheres'} value={adultos.mulheres} maxLen={4} inputMode={'numeric'}/>
-            <GuestComponent src={source.criancas} text={'Qtd. de crianças'} value={criancas} maxLen={4} inputMode={'numeric'}/>
-          </View>
+            {/* Convidados */}
+            <View style={S.stylesField.container}>
+              <Text style={S.stylesField.title}>Convidados:</Text>
+              <GuestComponent src={source.homem} text={'Qtd. de homens'} value={adultos.homens} maxLen={4} inputMode={'numeric'}/>
+              <GuestComponent src={source.mulher} text={'Qtd. de mulheres'} value={adultos.mulheres} maxLen={4} inputMode={'numeric'}/>
+              <GuestComponent src={source.criancas} text={'Qtd. de crianças'} value={criancas} maxLen={4} inputMode={'numeric'}/>
+            </View>
 
-          {/* Locação */}
-          <View style={S.stylesField.container}>
-            <Text style={S.stylesField.title}>Locação:</Text>
-            <OrganizerComponents width={'75%'} text={'Endereço:'} value={endereco.logradouro}/>
-            <OrganizerComponents width={'50%'} text={'Número:'} value={endereco.numero} inputMode={'numeric'}/>
-            <OrganizerComponents width={'50%'} text={'Bairro:'} value={endereco.bairro}/>
-            <OrganizerComponents width={'50%'} text={'Cidade:'} value={endereco.cidade}/>
-            <OrganizerComponents width={'30%'} text={'Estado:'} value={endereco.uf} maxLen={2} textTransform={'uppercase'}/>
-            <OrganizerComponents width={'35%'} text={'Valor de aluguel do espaço:'} value={locacao} inputMode={'decimal'}/>
+            {/* Locação */}
+            <View style={S.stylesField.container}>
+              <Text style={S.stylesField.title}>Locação:</Text>
+              <OrganizerComponents width={'75%'} text={'Endereço:'} value={endereco.logradouro}/>
+              <OrganizerComponents width={'50%'} text={'Número:'} value={endereco.numero} inputMode={'numeric'}/>
+              <OrganizerComponents width={'50%'} text={'Bairro:'} value={endereco.bairro}/>
+              <OrganizerComponents width={'50%'} text={'Cidade:'} value={endereco.cidade}/>
+              <OrganizerComponents width={'30%'} text={'Estado:'} value={endereco.uf} maxLen={2} textTransform={'uppercase'}/>
+              <OrganizerComponents width={'35%'} text={'Valor de aluguel do espaço:'} value={locacao} inputMode={'decimal'}/>
 
-          </View>
-          
-        </ScrollView>
+            </View>
+            
+          </ScrollView>
 
           <View style={S.stylesViewNext.container}>
             <Pressable style={S.stylesPressableNext.container}onPress={() => navigation.navigate('Carnes')}>
-              <Text style={S.stylesPressableNext.text}>{'>'}</Text>
+              <Text style={S.stylesPressableNext.text}>
+                <ChevronRight color="#fff" size={40} strokeWidth={2.0}/>
+              </Text>
             </Pressable>
           </View>
     

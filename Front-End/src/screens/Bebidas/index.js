bf451e259a842d74fe9bb3481e4a2846566d890f
GeoGustin <geogustinad@gmail.com>
2024-05-15 17:21:29 -0300

    Update DrinksSecreen

--- Front-End/src/screens/Bebidas/index.js
+++ Front-End/src/screens/Bebidas/index.js
@@ -1,4 +1,50 @@
-const DrinksScreen = () => {
+import { useState } from 'react';
+
+import { 
+  Text, 
+  View, 
+  Pressable, // Botão Personalizado
+  ScrollView,
+  Alert,
+} from 'react-native';
+
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+// inserção de área segura do dispositivo para Android e iOS.
+
+// Icon
+import { ChevronRight, Import } from 'lucide-react-native';
+
+import * as S from './style.js'
+
+import SelectItem from '../../components/SelectItem';
+
+
+const DrinksScreen = ({navigation}) => {
+
+    return(
+    <SafeAreaProvider style={S.stylesDrinksScreen.container}>
+        <ScrollView>
+
+        <View>
+            <Text style={S.stylesMainTitle.text}>Faça seu Cálculo</Text>
+        </View>
+
+        <View style={S.stylesField.container}>
+            <SelectItem/>
+        </View>
+
+        </ScrollView>
+
+        <View style={S.stylesViewNext.container}>
+            <Pressable style={S.stylesPressableNext.container}onPress={() => navigation.navigate('Bebidas')}>
+                <Text style={S.stylesPressableNext.text}>
+                    <ChevronRight color="#fff" size={40} strokeWidth={2.0}/>
+                </Text>
+            </Pressable>
+        </View>
+
+    </SafeAreaProvider>
+    )
     
 }
 

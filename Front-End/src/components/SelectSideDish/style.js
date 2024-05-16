06472fe916df3c1d766d2aaa8bda34cfb2c87f3c
GeoGustin <geogustinad@gmail.com>
2024-05-16 02:59:11 -0300

    Feat: create SelectSideDish component

---
+++ Front-End/src/components/SelectSideDish/style.js
@@ -0,0 +1,40 @@
+import { StyleSheet } from 'react-native'
+import { theme } from '../../theme'
+
+export const stylesComponent = StyleSheet.create({
+    container: {
+        display: 'flex',
+        flexDirection: 'column',
+        width: '100%',
+
+        gap: 20,
+    },
+    buttonGroup: {
+        display: 'flex',
+        flexDirection: 'row',
+        width: '100%',
+
+        justifyContent: 'space-between',
+    },
+    buttonGroupLast: {
+        display: 'flex',
+        flexDirection: 'row',
+        width: '100%',
+
+        justifyContent: 'center',
+    },
+    button: {
+        width: 140,
+        height: 90,
+        justifyContent: 'center',
+        borderRadius: theme.borderRadius.radius,
+    },
+    buttonSelected: {
+        backgroundColor: theme.colors.marquis_orange, // Cor quando seleciona
+    },
+    image: {
+        width: 70,
+        height: 70,
+        alignSelf: 'center',
+    },
+})

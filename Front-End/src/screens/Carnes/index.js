fd1aafac02098105e73866719a6db3dadb7503ce
doyklas <doyklas@gmail.com>
2024-06-05 15:31:00 -0300

    tentanto atualizar resultado

--- Front-End/src/screens/Carnes/index.js
+++ Front-End/src/screens/Carnes/index.js
@@ -1,24 +1,38 @@
-import React, { useState } from 'react';
-import { Text, View, Pressable, ScrollView } from 'react-native';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
-import { useNavigation } from '@react-navigation/native';
-import * as S from './style.js';
-import { ChevronRight, icons } from 'lucide-react-native';
+import { useState } from "react";
+
+import {
+  Text,
+  View,
+  Pressable, // Botão Personalizado
+  ScrollView,
+  Alert,
+} from "react-native";
+
+import { SafeAreaProvider } from "react-native-safe-area-context";
+// inserção de área segura do dispositivo para Android e iOS.
+
+// Icon
+import { ChevronRight } from "lucide-react-native";
+
+import * as S from "./style.js";
+import { theme } from "../../theme/index.js";
+import IconItem from "../../components/IconItem/index.js";
+import { Checkbox } from "react-native-paper";
 
 const MeatScreen = () => {
-  const navigation = useNavigation();
+  // const navigation = useNavigation();
   const [carne, setCarne] = useState([]);
 
   const handleNextPress = () => {
-    navigation.navigate('Drinks', {
-      carnesSelecionadas: carne
+    navigation.navigate("Drinks", {
+      carnesSelecionadas: carne,
     });
   };
 
   // Função para manipular a seleção de carne
   const handleCheckboxPress = (value) => {
     if (carne.includes(value)) {
-      setCarne(carne.filter(item => item !== value));
+      setCarne(carne.filter((item) => item !== value));
     } else {
       setCarne([...carne, value]);
     }
@@ -27,11 +41,11 @@ const MeatScreen = () => {
   // Componente para representar as opções de carne
   const MeatOption = ({ src, label }) => (
     <View style={S.stylesMeatOptions.container}>
-      <IconItem src={src}/>
+      <IconItem src={src} />
       {/* Checkbox carnes */}
       <View style={S.stylesCheckbox.container}>
         <Checkbox.Item
-          status={carne.includes(label) ? 'checked' : 'unchecked'}
+          status={carne.includes(label) ? "checked" : "unchecked"}
           onPress={() => handleCheckboxPress(label)}
           color={theme.colors.marquis_orange}
         />
@@ -46,12 +60,12 @@ const MeatScreen = () => {
         <View>
           <Text style={S.stylesMainTitle.text}>Escolha as Carnes</Text>
         </View>
-        
+
         {/* Tipo de carne */}
         <View style={S.stylesField.container}>
           <Text style={S.stylesField.title}>Tipo de Carne:</Text>
           <View style={S.stylesMeats.container}>
-            <MeatOption src={icons.boi} label="Picanha" />
+            <MeatOption src={image.boi} label="Picanha" />
             <MeatOption src={images.boi} label="Contra Filé" />
             <MeatOption src={images.boi} label="Coxão Mole" />
             {/* Adicione mais opções conforme necessário */}
@@ -66,6 +80,6 @@ const MeatScreen = () => {
       </Pressable>
     </SafeAreaProvider>
   );
-}
+};
 
 export default MeatScreen;
